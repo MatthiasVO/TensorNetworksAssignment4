02c4917282f5bcf1993d70ff4e6c860204ee6e87
bobojef <florisvanseumeren@gmail.com>
2025-10-13 12:10:20 +0200

    Update test.m
    added image rank plots

--- test.m
+++ test.m
@@ -3,3 +3,16 @@ disp("Hallo world!!!!")
 A = 3;
 disp("vo voor badhoevedorp")
 
+clear
+clc
+IMG = imread("4.2.07.tiff");
+R = IMG(:,:,1);
+G = IMG(:,:,2);
+B = IMG(:,:,3);
+
+Sr = svd(double(R));
+Sg = svd(double(G));
+Sb = svd(double(B));
+
+semilogy([Sr Sg Sb])
+
