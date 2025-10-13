dad8aa279cb66fe5ec443b073f42a543f208695b
bobojef <florisvanseumeren@gmail.com>
2025-10-13 13:01:21 +0200

    added omega

--- test.m
+++ test.m
@@ -14,5 +14,33 @@ Sr = svd(double(R));
 Sg = svd(double(G));
 Sb = svd(double(B));
 
+%plot log scale 
 semilogy([Sr Sg Sb])
+
+
+
+omega_l = 512*512;
+subset_percentage = 0.50;
+
+omega = zeros(omega_l,1);
+subset = round(omega_l*subset_percentage);
+
+omega(1:subset) = 1;
+omega = omega(randperm(omega_l));
+omega = reshape(omega,size(R));
+
+imshow(omega)
+
+
+
+
+
+
+
+
+
+
+
+rand
+
 
